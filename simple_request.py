f157600e9ef131c86abd7dff987544e83240fae4
root <2225992901@qq.com>
2020-07-20 15:39:34 +0800

    update for web usage

--- simple_request.py
+++ simple_request.py
@@ -6,7 +6,7 @@ import requests
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
-KERAS_REST_API_URL = "http://localhost:5000/predict"
+KERAS_REST_API_URL = "http://47.100.63.158/predict"
 DATA_PATH = "test.txt"
 
 # load the input image and construct the payload for the request
@@ -15,13 +15,14 @@ data = open(DATA_PATH, "r").read()
 payload = {"data": data}
 
 # submit the request
-r = requests.post(KERAS_REST_API_URL, files=payload).json()
+r = requests.post(KERAS_REST_API_URL, files=payload)
+print(r)
 
 # ensure the request was sucessful
-
-if r["success"]:
-	print(r['TNe'])
+#
+#if r["success"]:
+#	print(r['TNe'])
 
 # otherwise, the request failed
-else:
-	print("Request failed")
+#else:
+#	print("Request failed")
